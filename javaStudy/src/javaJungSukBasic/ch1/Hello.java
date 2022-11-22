bff04339ea54db1884dcc81bcdbdb5c407bed923
gon <gonsland99@gmail.com>
2022-11-22 11:07:37 +0900

    자바 학습 hello

---
+++ javaStudy/src/javaJungSukBasic/ch1/Hello.java
@@ -0,0 +1,8 @@
+package javaJungSukBasic.ch1;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!!!");
+		System.out.println("Hello,/* 이건 주석처리 안됨 */ world!!!");
+	}
+}
